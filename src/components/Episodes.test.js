47d7ab8cc17d9bc1bc37329615f22300026eb93e
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-12-14 14:26:34 -0500

    Test 2: Episodes component shows data when rerendered with new data

--- src/components/Episodes.test.js
+++ src/components/Episodes.test.js
@@ -3,8 +3,38 @@ import { render, screen } from '@testing-library/react';
 
 import Episodes from './Episodes';
 
-const episodes = [];
+const testEpisodes = [
+    {
+        id: Date.now(),
+        image: {},
+        name: "Test 1",
+        season: 1,
+        number: 1,
+        summary: "<p>Test 1</p>",
+        runtime: 3
+    },
+    {
+        id: Date.now() + 1,
+        image: {},
+        name: "Test 2",
+        season: 2,
+        number: 1,
+        summary: "<p>Test 2</p>",
+        runtime: 49
+    }
+];
 
 test('Renders <Episodes> without errors', () => {
-    render(<Episodes episodes={episodes}/>);
+    render(<Episodes episodes={[]}/>);
+})
+
+test('Episodes component shows data when rerendered with new data', () => {
+    const { rerender } = render( <Episodes episodes={[]}/> );
+
+    let episodes = screen.queryAllByTestId("episode");
+    expect(episodes).toHaveLength(0);
+
+    rerender(<Episodes episodes={testEpisodes}/>);
+    episodes = screen.queryAllByTestId("episode");
+    expect(episodes).toHaveLength(2);
 })
